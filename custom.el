236661d58e0785b27242d495adc21ae7ab1b3343
Cole T. <coledt54@gmail.com>
2025-02-21 19:36:07 -0700

    org mode

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/school/junior/public_space_proj/essay_draft.org" "/home/kira/school/junior/econ/macro/U4/U4.org" "/home/kira/school/vex-org/organize.org" "/home/kira/org/personal.org" "/home/kira/org/vex.org" "/home/kira/org/todo.org"))
+   '("~/.config/doom/org/personal.org" "/home/kira/.config/doom/org/todo.org" "/home/kira/.config/doom/org/vex.org" "/home/kira/school/junior/public_space_proj/essay_draft.org" "/home/kira/school/junior/econ/macro/U4/U4.org" "/home/kira/school/vex-org/organize.org"))
  '(warning-suppress-types '((ox-latex) (ox-latex) (defvaralias) (lexical-binding))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
